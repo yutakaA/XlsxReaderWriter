3ddb9c014c7e7f9b8cbd4b968c065ddd9dc8cd3f
MARTIN Denis <denis@irlmobile.com>
2019-11-13 15:29:26 +0100

    Removed pod dir

--- Pods/Target Support Files/Pods-XlsxReaderWriter/Pods-XlsxReaderWriter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_XlsxReaderWriter : NSObject
-@end
-@implementation PodsDummy_Pods_XlsxReaderWriter
-@end
--- Pods/Target Support Files/Pods-XlsxReaderWriterTests/Pods-XlsxReaderWriterTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_XlsxReaderWriterTests : NSObject
-@end
-@implementation PodsDummy_Pods_XlsxReaderWriterTests
-@end
